f4125ef816bcd024b68de53d5f702b8e324c162a
luis.ospino <luis.ospino@zalvadora.com>
2025-05-02 11:33:28 -0500

    ✨ Agrega conversión de números enteros a flotantes

--- app/Models/Treatment.php
+++ app/Models/Treatment.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Treatment extends Model
 {
+    protected $casts = [
+        'price' => MoneyCast::class,
+    ];
+
     public function patient(): BelongsTo
     {
         return $this->belongsTo(Patient::class);
