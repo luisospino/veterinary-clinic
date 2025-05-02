76869c78c9076f1b74bd1da0105e0790b951de9c
luis.ospino <luis.ospino@zalvadora.com>
2025-05-02 11:42:37 -0500

    ✨ Agrega campos de visualizacion en la tabla de tratamientos

--- app/Filament/Resources/PatientResource/RelationManagers/TreatmentsRelationManager.php
+++ app/Filament/Resources/PatientResource/RelationManagers/TreatmentsRelationManager.php
@@ -44,6 +44,15 @@ class TreatmentsRelationManager extends RelationManager
             ->recordTitleAttribute('description')
             ->columns([
                 Tables\Columns\TextColumn::make('description'),
+                (
+                    Tables\Columns\TextColumn::make('price')
+                    ->money('EUR')
+                    ->sortable()
+                ),
+                (
+                    Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                ),
             ])
             ->filters([
                 //
